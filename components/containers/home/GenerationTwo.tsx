b1f87a5beafd81d14dc08976d5f8f2e538979d2d
Senior Coder <agilecoder2040@gmail.com>
2024-06-04 02:46:44 +0900

    updat version

--- components/containers/home/GenerationTwo.tsx
+++ components/containers/home/GenerationTwo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import genThumb from "@/public/images/Deso.png";
+import genThumb from "@/public/images/desoImage.png";
 
 const GenerationTwo = () => {
   return (
